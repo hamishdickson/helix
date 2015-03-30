c019e37e756c08277d7728b06242b3b11f779128
hamishdickson <hamish.dickson@gmail.com>
2015-03-30 21:09:09 +0100

    problem 2

---
+++ src/test/scala/rosalind/problem2/DnaNucleotidesTest.scala
@@ -0,0 +1,17 @@
+package rosalind.problem2
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by hamishdickson on 30/03/15.
+ *
+ */
+class DnaNucleotidesTest extends FlatSpec with Matchers {
+  "An rna converter" should "be able to convert dna to rna" in {
+    val dnaString: String = "GATGGAACTTGACTACGTAAATT"
+
+    val converter: DnaNucleotides = new DnaNucleotides(dnaString)
+
+    converter.convert should be ("GAUGGAACUUGACUACGUAAAUU")
+  }
+}
