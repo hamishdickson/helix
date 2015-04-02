3cac729817dd00e000f70e211cf421979c2c8d5e
hamishdickson <hamish.dickson@gmail.com>
2015-04-02 08:49:23 +0100

    lay out problem 4

---
+++ src/test/scala/rosalind/problem4/WabbitsTest.scala
@@ -0,0 +1,13 @@
+package rosalind.problem4
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Created by hamishdickson on 02/04/15.
+ *
+ */
+class WabbitsTest extends FlatSpec with Matchers {
+  "A single wrabbit pair" should "reproduce at a rate consistent with fibonacci" in {
+    
+  }
+}
