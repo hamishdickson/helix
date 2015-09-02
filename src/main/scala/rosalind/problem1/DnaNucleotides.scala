88a608cb3055e302a215944799c477b999c539a6
hamishdickson <hamish.dickson@gmail.com>
2015-09-02 18:58:06 +0100

    refactor count

--- src/main/scala/rosalind/problem1/DnaNucleotides.scala
+++ src/main/scala/rosalind/problem1/DnaNucleotides.scala
@@ -1,8 +1,6 @@
 package rosalind.problem1
 
 /**
- * Created by hamishdickson on 30/03/15.
- * 
  * A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length 
  * of a string is the number of symbols that it contains.
  * 
@@ -11,8 +9,18 @@ package rosalind.problem1
  * Create a function with the following attributes:
  * takes:  A DNA string s of length at most 1000 nt.
  * returns: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
- *
  */
 class DnaNucleotides(nucleotides: String) {
   def count(nuc: Char) = nucleotides.count(_ == nuc)
+
+  def count2(nuc: Char): Int = {
+   def loop(c: List[Char]): Int = c match {
+      case Nil => 0
+      case x :: xs => if (x == nuc) 1 + loop(xs) else loop(xs)
+    }
+
+    loop(nucsAsList)
+  }
+
+  val nucsAsList: List[Char] = nucleotides.toList
 }
