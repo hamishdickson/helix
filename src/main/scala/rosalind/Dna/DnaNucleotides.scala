24e346a7c89fba49772252491f042f1b2c19e1cf
hamishdickson <hamish.dickson@gmail.com>
2015-09-05 22:11:48 +0100

    count gc factor of a genome

--- src/main/scala/rosalind/Dna/DnaNucleotides.scala
+++ src/main/scala/rosalind/Dna/DnaNucleotides.scala
@@ -28,7 +28,7 @@ case class DnaNucleotides(nucleotides: List[Genome]) extends Dna {
    * Given: A DNA string t having length at most 1000 nt.
    * Return: The transcribed RNA string of t.
    */
-  def convert: Rna = RnaNucleotides(nucleotides.map {
+  def convert: Rna = RnaNucleotides(nucleotides map {
     case GenomeA => RnaGenomeA
     case GenomeC => RnaGenomeC
     case GenomeG => RnaGenomeG
@@ -50,6 +50,29 @@ case class DnaNucleotides(nucleotides: List[Genome]) extends Dna {
     case GenomeC => GenomeG
     case GenomeG => GenomeC
   })
+
+
+/**
+ * The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example,
+ * the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
+ *
+ * DNA strings must be labeled when they are consolidated into a database. A commonly used method of string labeling is
+ * called FASTA format. In this format, the string is introduced by a line that begins with '>', followed by some
+ * labeling information. Subsequent lines contain the string itself; the first line to begin with '>' indicates the
+ * label of the next string.
+ *
+ * In Rosalind's implementation, a string in FASTA format will be labeled by the ID "Rosalind_xxxx", where "xxxx"
+ * denotes a four-digit code between 0000 and 9999.
+ *
+ * Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+ * Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind
+ * allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute
+ * error below.
+ */
+  def cgCounter: Double = {
+    val count: Double = nucleotides.foldRight(0)((a, b) => if (a == GenomeC || a == GenomeG) 1 + b else b)
+    100 * count / nucleotides.length
+  }
 }
 
 object Dna {
