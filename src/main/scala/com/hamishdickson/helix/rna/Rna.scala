78a9f63f9e2af63a5c630c8540b5f5eada96c258
hamishdickson <hamish.dickson@gmail.com>
2015-10-07 21:26:25 +0100

    code clean

--- src/main/scala/com/hamishdickson/helix/rna/Rna.scala
+++ src/main/scala/com/hamishdickson/helix/rna/Rna.scala
@@ -1,16 +1,9 @@
 package com.hamishdickson.helix.rna
 
-import com.hamishdickson.helix.protein.Protein
-
 trait Rna
 
-case class RnaGenome(nucleotides: List[RnaNucleotide]) extends Rna {
-  def toProteinList: List[Protein] = ???
-}
+case class RnaGenome(nucleotides: List[RnaNucleotide]) extends Rna
 
 object Rna {
-  /**
-   * Annoyingly, this data is typically stored as a string
-   */
   def apply(s: String): RnaGenome = RnaGenome(s.toList.map(g => RnaNucleotide(g)))
 }
--- src/test/scala/com/hamishdickson/helix/rna/RnaGenomeTest.scala
+++
@@ -1,6 +0,0 @@
-package com.hamishdickson.helix.rna
-
-import org.scalatest.{FlatSpec, Matchers}
-
-class RnaGenomeTest extends FlatSpec with Matchers {
-}
