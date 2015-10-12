2236991086aed7a75590071b38ba862f7d072fa6
hamishdickson <hamish.dickson@gmail.com>
2015-10-12 18:58:58 +0100

    add description for consensus

--- src/main/scala/com/hamishdickson/helix/dna/Dna.scala
+++ src/main/scala/com/hamishdickson/helix/dna/Dna.scala
@@ -133,6 +133,23 @@ object Dna {
     ((k*k - k) + 2*(k*m) + 2*(k*n) + (.75*(m*m - m)) + 2*(.5*m*n))/tot
   }
 
+  /**
+   * A matrix is a rectangular table of values divided into rows and columns. An m×n matrix has m rows and n columns.
+   * Given a matrix A, we write Ai,j to indicate the value found at the intersection of row i and column j.
+   *
+   * Say that we have a collection of DNA strings, all having the same length n. Their profile matrix is a 4×n matrix
+   * P in which P1,j represents the number of times that 'A' occurs in the jth position of one of the strings, P2,j
+   * represents the number of times that C occurs in the jth position, and so on (see below).
+   *
+   * A consensus string c is a string of length n formed from our collection by taking the most common symbol at each
+   * position; the jth symbol of c therefore corresponds to the symbol having the maximum value in the j-th column of
+   * the profile matrix. Of course, there may be more than one most common symbol, leading to multiple possible
+   * consensus strings.
+   *
+   * Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
+   * Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist,
+   * then you may return any one of them.)
+   */
   def consensus(genomes: List[Genome]): Genome = {
     val a: List[List[Nucleotide]] = genomes.map(_.nucleotides).transpose
 
@@ -143,7 +160,7 @@ object Dna {
     Genome(b)
   }
 
-
+  @deprecated("formattedConsensus is very inefficient and used for testing only.")
   def formattedConsensus(genomes: List[Genome]): String = {
     val x: Genome = consensus(genomes)
 
@@ -161,7 +178,7 @@ object Dna {
     "\nT: " + a(3).mkString(" ")
   }
 
-  def asString(g: Genome): String = g.nucleotides.map(_.asString).mkString("")
+  private def asString(g: Genome): String = g.nucleotides.map(_.asString).mkString("")
 
   private def toTupleList(a: List[List[Nucleotide]]): List[(Int, Int, Int, Int)] = for {
     x <- a
