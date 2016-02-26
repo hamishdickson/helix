83fc69a3974ede64cd93c0aec59aeea94cbd701b
hamishdickson <hamish.dickson@gmail.com>
2016-02-26 20:12:51 +0000

    start refactoring out common patterns

---
+++ src/main/scala/com/hamishdickson/helix/genomes/Genome.scala
@@ -0,0 +1,18 @@
+package com.hamishdickson.helix.genomes
+
+import scala.language.higherKinds
+
+/**
+  * Common DNA/RNA/mRNA things
+  */
+sealed trait Genome[F[_]]
+case class GenomeString[A](g: List[A]) extends Genome {
+
+  /**
+    * How many times does nucleotide n occur in genome g ?
+    */
+  def count[B](n: B): Int = g.foldRight(0)((h, t) => if (h == n) 1 + t else t)
+
+  def hemmingDistance(t: GenomeString[A]): Int =
+    (g zip t.g).foldRight(0){ case ((i,j), b) => if (i == j) b else 1 + b }
+}
