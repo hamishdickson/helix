73d5d100e46a76b6a586ced912533c5a2ba9ad3a
hamishdickson <hamish.dickson@gmail.com>
2016-05-23 20:02:35 +0100

    scalaz => cats

--- build.sbt
+++ build.sbt
@@ -2,10 +2,10 @@ name := "helix"
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
-  "org.scalaz" %% "scalaz-core" % "7.2.0"
+  "org.typelevel" %% "cats" % "0.6.0",
+  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 )
 
--- src/main/scala/com/hamishdickson/helix/genomes/Genome.scala
+++ src/main/scala/com/hamishdickson/helix/genomes/Genome.scala
@@ -1,18 +1,17 @@
 package com.hamishdickson.helix.genomes
 
+import cats.Foldable
+
 import scala.language.higherKinds
 
 /**
   * Common DNA/RNA/mRNA things
   */
-sealed trait Genome[+A]
-case class GenomeString[A](g: List[A]) extends Genome[A] {
-
+trait Genome[F[_]] extends Foldable[F] {
   /**
     * How many times does nucleotide n occur in genome g ?
     */
-  def count[B](n: B): Int = g.foldRight(0)((h, t) => if (h == n) 1 + t else t)
+  def occurances[A](n: A): Int
 
-  def hemmingDistance(t: GenomeString[A]): Int =
-    (g zip t.g).foldRight(0){ case ((i,j), b) => if (i == j) b else 1 + b }
+  def hemmingDistance(t: Genome[F[_]]): Int
 }
