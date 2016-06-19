4b53c7ca09979d1d05ae73719459846a9a1f9a7c
hamishdickson <hamish.dickson@gmail.com>
2016-06-19 18:01:00 +0100

    clean up sillyness

--- src/main/scala/com/hamishdickson/helix/genomes/Genome.scala
+++ src/main/scala/com/hamishdickson/helix/genomes/Genome.scala
@@ -1,17 +1,9 @@
 package com.hamishdickson.helix.genomes
 
-import cats.Foldable
-
 import scala.language.higherKinds
 
 /**
   * Common DNA/RNA/mRNA things
   */
-trait Genome[F[_]] extends Foldable[F] {
-  /**
-    * How many times does nucleotide n occur in genome g ?
-    */
-  def occurances[A](n: A): Int
-
-  def hemmingDistance(t: Genome[F[_]]): Int
+trait Genome[F[_]] {
 }
